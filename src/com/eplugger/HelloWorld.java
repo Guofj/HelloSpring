94abb1295600cab3c1fbbf57727746cd174cada9
Guofj <guofujie@e-plugger.com>
2017-08-31 09:16:21 +0800

    no change no change

--- src/com/eplugger/HelloWorld.java
+++ src/com/eplugger/HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
 	public void setMessage(String message){
 		this.message = message;
 	}
-	
+
 	public void getMessage(){
 		System.out.println("Your Message:"+message+"!");
 	}
